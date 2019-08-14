2c19a9a8c8f66f5c0234e98bbfaba848f8c6431b
natedogg484 <nathangschmidt@gmail.com>
2019-08-14 12:34:41 -0400

    server up and running

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express();
 const router = express.Router();
-const db = require('/db');
+const db = require('./db');
 
 
 const path = __dirname + '/public/';
+const PORT = 8080;
+
+router.use(function (req, res, next) {
+    console.log('/', req.method);
+    next;
+})
+
+router.get('/', function(req, res) {
+    res.sendFile(path + 'index.html'); 
+});
+
+router.get('/', function(req, res) {
+    res.sendFile(path + 'forceUsers.html');
+});
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
